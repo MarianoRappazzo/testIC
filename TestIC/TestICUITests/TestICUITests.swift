7b8c1fbcdbd8b3b96946a26fe533a6d7656f060b
MarianoRappazzo <mrappazzo@despegar.com>
2021-03-12 17:57:13 -0300

    UItests test

--- TestIC/TestICUITests/TestICUITests.swift
+++ TestIC/TestICUITests/TestICUITests.swift
@@ -26,17 +26,22 @@ class TestICUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let holaMundoStaticText = app.staticTexts["Hola Mundo!"]
+        holaMundoStaticText.tap()
+        
+        let element = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.tap()
+        
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testExample2() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        let holaMundoStaticText = app.staticTexts["Hola!"]
+        holaMundoStaticText.tap()
+        
     }
 }
