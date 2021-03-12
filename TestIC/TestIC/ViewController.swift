9a1a5d920ce3918dc1b7452e1bdeafc0149ad8ae
MarianoRappazzo <mrappazzo@despegar.com>
2021-03-12 13:38:45 -0300

    Initial Commit

---
+++ TestIC/TestIC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestIC
+//
+//  Created by Mariano on 12/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
